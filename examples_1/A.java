e1d8591e3a333ff4df231d5b8965a2590b554ed3
Ellen Kartysheva <el.kartysheva@yandex.ru>
2020-07-22 23:19:02 +0300

    Add part of lecture 1 and example

---
+++ examples_1/A.java
@@ -0,0 +1,11 @@
+package examples_1;
+
+public class A {
+    public int a;
+    public A () {
+        a = 10;
+    }
+    private A (int a) {
+        this.a = a;
+    }
+}
---
+++ examples_1/Main.java
@@ -0,0 +1,8 @@
+package examples_1;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        // A privateA = new A(10); ошибка компиляции
+    }
+}
